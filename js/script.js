043e4307dc50ceaf08529f1db2236f28dd665e0c
Dmitriy Shapovalov <shapovalov-84@internet.ru>
2024-01-22 16:04:41 +0700

    first app

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+// const a = prompt('Один из последних просмотренных фильмов?');
+// const b = prompt('На сколько оцените его?');
+// const c = prompt('Один из последних просмотренных фильмов?');
+// const d = prompt('На сколько оцените его?');
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// ================================================================
+// const myName = prompt('Как Ваше имя?'),
+//   lastName = prompt('Как Ваша фамилия?'),
+//   age = +prompt('Сколько Вам лет?'),
+//   children = prompt('Есть ли у Вас дети?'),
+//   childName = prompt('Как зовут Вашего ребенка?'),
+//   childAge = +prompt('сколько лет Вашему ребенку?'),
+//   childName1 = prompt('Как зовут Вашего ребенка?'),
+//   childAge1 = +prompt('сколько лет Вашему ребенку?');
+
+
+// const person = {
+
+// };
+// person['name'] = myName;
+// person['lastName'] = lastName;
+// person['age'] = age;
+// person['children'] = children;
+// person.chidren = {};
+// person.chidren[childName] = childAge;
+// person.chidren[childName1] = childAge1;
+// console.log(person);
+
+// =================================================================
+/*
+let array = [];
+for (let i = 0; i < 4; i++) {
+  const answer = prompt('Как Ваше имя?');
+  array.push(`${answer}`);
+}
+console.log(array);
+*/
